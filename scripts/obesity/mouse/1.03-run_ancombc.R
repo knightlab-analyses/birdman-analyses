bd47eab0495c63f70b6e098f3629598c4660a68d
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-08-23 11:33:53 -0700

    add more scripts

--- scripts/obesity/mouse/1.03-run_ancombc.R
+++ scripts/obesity/mouse/1.03-run_ancombc.R
@@ -1,6 +1,6 @@
 #!/home/grahman/miniconda3/envs/da-R/bin/Rscript
 #SBATCH --chdir=/home/grahman/projects/birdman-analyses-final
-#SBATCH --output=/home/grahman/projects/birdman-analyses-final/slurm_out/obesity/%x.out
+#SBATCH --output=/home/grahman/projects/birdman-analyses-final/slurm_out/obesity/mouse/%x.out
 #SBATCH --partition=short
 #SBATCH --mem=8G
 #SBATCH --nodes=1
@@ -11,11 +11,11 @@ library(ANCOMBC)
 library(biomformat)
 library(phyloseq)
 
-tbl_file <- "data/obesity/processed/processed_tbl.genus.biom"
+tbl_file <- "data/obesity/processed/mouse/tbl_merged.genus.biom"
 tbl <- biomformat::read_biom(tbl_file)
 tbl <- as.matrix(biomformat::biom_data(tbl))
 
-md_file <- "data/obesity/processed/processed_md.tsv"
+md_file <- "data/obesity/processed/mouse/metadata.merged.tsv"
 md <- read.table(md_file, sep="\t", row.names=1, header=T)
 md$diet <- relevel(as.factor(md$diet), "Standard")
 md$instrument <- relevel(as.factor(md$instrument), "Illumina")
@@ -35,5 +35,5 @@ ancombc.results <- ANCOMBC::ancombc(phyloseq=physeq, formula=design_formula,
                                     zero_cut=1.0)
 results <- as.data.frame(ancombc.results$res)
 
-outfile <- "results/obesity/ancombc_results.tsv"
+outfile <- "results/obesity/mouse/ancombc_results.tsv"
 write.table(results, file=outfile, sep="\t")
