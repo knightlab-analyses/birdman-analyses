8fe984992d0b92b876ddb97fda035baa3e407289
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-09-19 11:41:12 -0700

    tcga figure & nb

--- scripts/tcga/2.03-summarize_birdman.center.py
+++ scripts/tcga/2.03-summarize_birdman.center.py
@@ -16,40 +16,20 @@ import pandas as pd
 FEAT_REGEX = re.compile("F\d{4}_(.*).nc")
 
 inference_dir = "/panfs/grahman/birdman-analyses-final/tcga/species/bacteria"
-outfile = "results/tcga/species/birdman_results.bacteria.center.tsv"
+outfile = "results/tcga/species/birdman_results.bacteria.center.nc"
 all_inf_files = glob.glob(f"{inference_dir}/*.nc")
 
-def process_dataframe(df, feat_id, suffix=""):
-    df = df.copy()
-    df.reset_index(inplace=True, drop=True)
-    df.columns.name = ""
-    df.index = [feat_id]
-    df.columns = [x + suffix for x in df.columns]
-    return df
-
-feat_diff_df_list = []
+feat_inf_list = []
+feat_names = []
 for inf_file in all_inf_files:
     this_feat_id = FEAT_REGEX.search(inf_file).groups()[0]
-    this_feat_diff = az.from_netcdf(inf_file).posterior["center_re"]
-    this_feat_diff_mean = this_feat_diff.mean(["chain", "draw"]).to_dataframe().T
-    this_feat_diff_std = this_feat_diff.std(["chain", "draw"]).to_dataframe().T
+    this_feat_inf = az.from_netcdf(inf_file).posterior["center_re"]
 
-    this_feat_diff_mean = process_dataframe(
-        this_feat_diff_mean,
-        this_feat_id,
-        suffix="_mean"
-    )
-    this_feat_diff_std = process_dataframe(
-        this_feat_diff_std,
-        this_feat_id,
-        suffix="_std"
-    )
-    this_feat_diff_df = pd.concat(
-        [this_feat_diff_mean, this_feat_diff_std],
-        axis=1
-    )
-    feat_diff_df_list.append(this_feat_diff_df)
+    feat_inf_list.append(this_feat_inf)
+    feat_names.append(this_feat_id)
 
-all_feat_diffs_df = pd.concat(feat_diff_df_list, axis=0)
-all_feat_diffs_df.index.name = "Feature"
-all_feat_diffs_df.to_csv(outfile, sep="\t", index=True)
+full_inf = xr.concat(
+    feat_inf_list,
+    pd.Index(feat_names, name="feature"),
+)
+full_inf.to_netcdf(outfile)
