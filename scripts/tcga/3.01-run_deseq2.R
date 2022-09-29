9fa6b044388bdfc6e27c8e91434ee54b04658176
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-09-29 08:18:47 -0700

    tcga r scripts

---
+++ scripts/tcga/3.01-run_deseq2.R
@@ -0,0 +1,57 @@
+#!/home/grahman/miniconda3/envs/da-R/bin/Rscript
+#SBATCH --chdir=/home/grahman/projects/birdman-analyses-final
+#SBATCH --output=/home/grahman/projects/birdman-analyses-final/slurm_out/tcga/%x.out
+#SBATCH --partition=short
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=6:00:00
+
+library(biomformat)
+library(DESeq2)
+
+tbl_file <- "data/tcga/processed/species/processed_tbl.bacteria.biom"
+tbl <- biomformat::read_biom(tbl_file)
+tbl <- as.matrix(biomformat::biom_data(tbl))
+
+md_file <- "data/tcga/processed/processed_md.tsv"
+md <- read.delim(md_file, sep="\t", row.names=1, header=T)
+md$investigation <- gsub("TCGA-", "", md$investigation)
+md$investigation <- relevel(as.factor(md$investigation), "BRCA")
+md$race <- as.factor(md$race)
+md$gender <- as.factor(md$gender)
+
+samples <- colnames(tbl)
+md <- subset(md, rownames(md) %in% samples)
+sample_order <- row.names(md)
+
+tbl <- tbl[, sample_order]
+
+design.formula <- as.formula("~investigation + race + gender")
+
+dds <- DESeq2::DESeqDataSetFromMatrix(
+    countData=tbl,
+    colData=md,
+    design=design.formula
+)
+dds_results <- DESeq2::DESeq(dds, sfType="poscounts")
+
+levels <- unique(md$investigation)
+
+for (level in levels) {
+    print(level)
+    if (level == "BRCA") {
+        print("Skipping")
+        next
+    }
+    results <- DESeq2::results(
+        dds_results,
+        format="DataFrame",
+        tidy=TRUE,
+        cooksCutoff=FALSE,
+        contrast=c("investigation", level, "BRCA")
+    )
+    row.names(results) <- results$row
+    outfile <- paste0("results/tcga/deseq2/", level, ".tsv")
+    write.table(results, file=outfile, sep="\t")
+}
